42f563296bbcff42ddd1189a5a553fbe032943d3
Claudio Martin Bianco <39170327+claudiomartinbianco@users.noreply.github.com>
2021-06-22 10:40:25 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Developer News</h1>
+
+<?php echo "The Best PHP Examples"; ?>
+  
+<h2>Developer News</h2>
+  
+ <?php
+$name = "Quincy";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+?>
+
+</body>
+</html> 
