79366a6bad0a428dae0ab86f4fc51fa1dd1251a1
Claudio Martin Bianco <39170327+claudiomartinbianco@users.noreply.github.com>
2021-06-22 14:16:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,20 +22,6 @@ app.controller('customersCtrl', function($scope, $http) {
   });
 });
 </script>
-  
-  
-<h1>Developer News</h1>
-
-<?php echo "The Best PHP Examples"; ?>
-  
-<h2>Developer News</h2>
-  
- <?php
-$name = "Quincy";
-echo "Hi! My name is " . $name . "<br>";
-echo "Hi! My name is " . $NAME . "<br>";
-echo "Hi! My name is " . $NaMe . "<br>";
-?>
 
 </body>
 </html> 
